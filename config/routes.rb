e4a9710993d80912dedb6557613822a8ad698a7c
rungo728 <rufii72865@gmail.com>
2019-10-03 20:18:04 +0900

    Update products route and controller,view
    トップページを商品詳細ページに変更productsのアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'products#index'
+  resources :products, only: [:index, :show, :new]
   resources :users, only: :show
-  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
