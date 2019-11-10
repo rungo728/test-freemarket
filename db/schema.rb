86b8162abd60d6a0f6a6012b5aec4325dfdf81ea
rungo728 <rufii72865@gmail.com>
2019-11-10 14:05:44 +0900

    create prefecture table
    新規登録の３つ目発送先住所の都道府県のデータをprefecturesteテーブルを作って呼び出すため、まずはprefectureのモデル作成とアソシエーションを組んで、必要なカラムを設定し、rake db:migrateした。DBにprefectureテーブルができた。あとはnameカラムに都道府県名を入れるだけ。

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191002124646) do
+ActiveRecord::Schema.define(version: 20191102105542) do
+
+  create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "nickname",                                          null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "last_name",                                         null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name_kana",                                    null: false
+    t.string   "first_name_kana",                                   null: false
+    t.date     "birthday",                                          null: false
+    t.string   "phone_number",                                      null: false
+    t.string   "postcode",                                          null: false
+    t.string   "prefecture",                                        null: false
+    t.string   "city",                                              null: false
+    t.string   "address",                                           null: false
+    t.string   "building"
+    t.string   "phone_number_sub"
+    t.text     "profile",                limit: 65535
+    t.integer  "point",                                             null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
