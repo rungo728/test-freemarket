8c46c5c4c53b42011413676cf20147599b47da0e
rungo728 <rufii72865@gmail.com>
2019-11-10 12:20:49 +0900

    create signup step1-5
    新規登録全て画面のマークアップをhamlとscssにて行い、それに必要なルーティングとコントローラーの設定を行った

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,22 @@
 class SignupController < ApplicationController
+
   def step1
-    
+    # 新規インスタンス作成
+    @user = User.new
+  end
+
+  def step2_1
+  end
+
+  def step2_2
+  end
+
+  def step3
+  end
+  
+  def step4
+  end
+
+  def step5 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :signup do
     collection do
       get 'step1'
-      get 'step2'
+      get 'step2_1'
+      get 'step2_2'
       get 'step3'
       get 'step4'
       get 'step5'
