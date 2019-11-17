76ddb6cbff441d9763225fcf4be3cb1580d3facf
rungo728 <rufii72865@gmail.com>
2019-11-17 10:53:26 +0900

    set controller and view
    試しにテーブルに保存されている情報がトップページの商品一覧に表示されるかレディースの部分だけテストしてみた。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    #テストで用いた記述
+    @items = Item.all.order("id DESC").limit(10)
+    #カテゴリーごとに並べる
+    @ladies = Item.includes(:photos).where(category_id: 0..199).order("id DESC").limit(10)
   end
 
   def new
